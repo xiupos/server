ecd11f31e746606456d9a76f2ba85ce198f510c7
xiupos <me@xiupos.net>
2024-09-23 19:11:41 +0900

    update

--- workflow/backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR=`cd $(dirname ${0}) && pwd`
-source $DIR/.config/docker.env
-
-# Create backup
-mkdir -p $DIR/backup
-tar -C $DIR -zcvf backup/data.tar.gz data
--- workflow/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR=`cd $(dirname ${0}) && pwd`
-
-docker-compose -f $DIR/docker-compose.yml pull
-docker-compose -f $DIR/docker-compose.yml down
-docker-compose -f $DIR/docker-compose.yml up -d
