a5cf79df1baa8b540b7affe858efa637a5753c24
xiupos <me@xiupos.net>
2024-09-23 23:30:23 +0900

    update 11 files

--- misskey-phys/restore.sh
+++ misskey-phys/restore.sh
@@ -2,9 +2,9 @@
 
 DIR=`cd $(dirname ${0}) && pwd`
 
-docker-compose -f $DIR/docker-compose.yml down
+docker compose -f $DIR/docker compose.yml down
 sudo rm -rf $DIR/data
 
 sudo tar -C $DIR -zxvf $DIR/backup/data.tar.gz data
 
-# docker-compose -f $DIR/docker-compose.yml up -d
+# docker compose -f $DIR/docker compose.yml up -d
