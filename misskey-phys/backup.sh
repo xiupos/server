a375c1ce11fc4fe2f436f6f7a84297f0a77fd9a6
xiupos <me@xiupos.net>
2025-05-03 19:41:12 +0900

    remove phys

--- misskey-phys/backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIR=`cd $(dirname ${0}) && pwd`
-source $DIR/.config/docker.env
-
-# Create backup
-mkdir -p $DIR/backup
-docker-compose -f $DIR/docker-compose.yml exec -T db \
-  pg_dumpall -U $POSTGRES_USER | gzip -c > $DIR/backup/pgdumpall.gz
