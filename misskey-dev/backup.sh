7dacd334544f2067f489145eaf4bb10b5a7e9125
xiupos <me@xiupos.net>
2025-08-28 09:16:20 +0900

    update backup.sh and restore.sh

--- misskey-dev/backup.sh
+++ misskey-dev/backup.sh
@@ -5,5 +5,10 @@ source $DIR/.config/docker.env
 
 # Create backup
 mkdir -p $DIR/backup
+
+# postgresql
 docker-compose -f $DIR/docker-compose.yml exec -T db \
   pg_dumpall -U $POSTGRES_USER | gzip -c > $DIR/backup/pgdumpall.gz
+
+# redis
+sudo tar -C $DIR -zcvf backup/redis.tar.gz data/redis
--- misskey-dev/restore.sh
+++ misskey-dev/restore.sh
@@ -5,11 +5,15 @@ DIR=`cd $(dirname ${0}) && pwd`
 docker compose -f $DIR/docker-compose.yml down
 sudo rm -rf $DIR/data
 
+# postgresql
 docker-compose -f $DIR/docker-compose.yml up -d db && sleep 5
 gunzip -c -d $DIR/backup/pgdumpall.gz > $DIR/pgdumpall
 docker-compose -f $DIR/docker-compose.yml exec -T db \
   psql -U $POSTGRES_USER $POSTGRES_DB < $DIR/pgdumpall
 rm $DIR/pgdumpall
 docker-compose -f $DIR/docker-compose.yml stop db
+
+# redis
+sudo tar -C $DIR -zxvf $DIR/backup/redis.tar.gz redis
 
 # docker compose -f $DIR/docker-compose.yml up -d
