a287694ade0695cbceb6255f1576a9c4870c2653
xiupos <me@xiupos.net>
2025-08-28 12:07:53 +0900

    Revert "update backup.sh, restore.sh, backup.sh and restore.sh"
    This reverts commit 82346c8cb2f329b09526e2d759a99061e5166033.

--- misskey/backup.sh
+++ misskey/backup.sh
@@ -5,10 +5,5 @@ source $DIR/.config/docker.env
 
 # Create backup
 mkdir -p $DIR/backup
-
-# postgresql
 docker-compose -f $DIR/docker-compose.yml exec -T db \
   pg_dumpall -U $POSTGRES_USER | gzip -c > $DIR/backup/pgdumpall.gz
-
-# redis
-sudo tar -C $DIR -zcvf backup/redis.tar.gz data/redis
