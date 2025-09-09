aef8cc51a9c54bd943a4f272bfd19540298b4944
xiupos <me@xiupos.net>
2025-09-09 18:49:11 +0900

    update 6 files

--- misskey-dev/backup.sh
+++ misskey-dev/backup.sh
@@ -5,5 +5,11 @@ source $DIR/.config/docker.env
 
 # Create backup
 mkdir -p $DIR/backup
+
+# postgresql
 docker-compose -f $DIR/docker-compose.yml exec -T db \
   pg_dumpall -U $POSTGRES_USER | gzip -c > $DIR/backup/pgdumpall.gz
+
+# redis
+sudo tar -C $DIR -zcvf backup/redis.tar.gz data/redis
+sudo chown -c xiupos:xiupos $DIR/backup/redis.tar.gz
