2eb74da1aed5f79829f316ea4e1e362fd8f4b9a6
xiupos <me@xiupos.net>
2025-09-09 18:54:40 +0900

    update vacuum-full.sh, vacuum-full.sh and vacuum-full.sh

--- misskey/vacuum-full.sh
+++ misskey/vacuum-full.sh
@@ -4,6 +4,6 @@ DIR=`cd $(dirname ${0}) && pwd`
 source $DIR/.config/docker.env
 
 docker compose -f $DIR/docker-compose.yml up -d
-docker compose -f $DIR/docker-compose.yml misskey down
+docker compose -f $DIR/docker-compose.yml down misskey
 docker compose -f $DIR/docker-compose.yml exec db psql -U $POSTGRES_USER -d $POSTGRES_DB -c "vacuum full;"
 docker compose -f $DIR/docker-compose.yml up -d
