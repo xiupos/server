ed03032cead63699090115ce9510fae2b1f7bb5b
xiupos <me@xiupos.net>
2025-12-06 22:59:36 +0900

    update backup.sh and restore.sh

--- misskey/backup.sh
+++ misskey/backup.sh
@@ -11,5 +11,5 @@ docker-compose -f $DIR/docker-compose.yml exec -T db \
   pg_dumpall -U $POSTGRES_USER | gzip -c > $DIR/backup/pgdumpall.gz
 
 # redis
-sudo tar -C $DIR -zcvf backup/redis.tar.gz data/redis
-sudo chown -c xiupos:xiupos $DIR/backup/redis.tar.gz
+# sudo tar -C $DIR -zcvf backup/redis.tar.gz data/redis
+# sudo chown -c xiupos:xiupos $DIR/backup/redis.tar.gz
--- misskey/restore.sh
+++ misskey/restore.sh
@@ -15,6 +15,6 @@ rm $DIR/pgdumpall
 docker-compose -f $DIR/docker-compose.yml stop db
 
 # redis
-sudo tar -C $DIR -zxvf $DIR/backup/redis.tar.gz data/redis
+# sudo tar -C $DIR -zxvf $DIR/backup/redis.tar.gz data/redis
 
 # docker compose -f $DIR/docker-compose.yml up -d
