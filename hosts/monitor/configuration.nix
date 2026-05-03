7fa771a65ecd30f2ec7e71ff15f898a9deb3b898
xiupos <me@xiupos.net>
2026-05-03 17:22:29 +0900

    update configuration.nix

--- hosts/monitor/configuration.nix
+++ hosts/monitor/configuration.nix
@@ -2,17 +2,15 @@
   imports = [];
 
   # Networking
-  networking = {
-    hostName = "monitor";
+  networking.hostName = "monitor";
 
+  networking = {
     interfaces.eth0.ipv4.addresses = [{
       address = "10.0.0.17";
       prefixLength = 24;
     }];
-
     defaultGateway = "10.0.0.1";
   };
-
 
   # System state version
   system.stateVersion = "25.11";
