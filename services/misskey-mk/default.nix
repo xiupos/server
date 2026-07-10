0de224a6a5120aff4966d493db8ec26759cf6c59
xiupos <me@xiupos.net>
2026-07-10 09:22:20 +0900

    fix(misskey-mk): set max_wal_size

--- services/misskey-mk/default.nix
+++ services/misskey-mk/default.nix
@@ -70,7 +70,10 @@ in {
         host  all all 127.0.0.1/32 trust
         host  all all ::1/128 trust
       '';
-      settings.shared_buffers = cfg.sharedBuffers;
+      settings = {
+        shared_buffers = cfg.sharedBuffers;
+        max_wal_size = "4GB";
+      };
     };
   };
 }
